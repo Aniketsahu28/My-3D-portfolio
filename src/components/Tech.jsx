a77dc2e91ba93bf8d9b50b01713463dbe31205e2
Aniket <aniketsahu1711@gmail.com>
2023-05-03 16:03:46 +0530

    Done with tech section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,11 +6,11 @@ import { technologies } from "../constants";
 const Tech = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology) => {
+      {technologies.map((technology) => (
         <div className="w-28 h-28" key={technology.name}>
           <BallCanvas icon={technology.icon} />
         </div>
-      })}
+      ))}
     </div>
   )
 }
