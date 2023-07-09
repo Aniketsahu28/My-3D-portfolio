2c5fa7d3e4897fda32b828b93dd7a7d5c84226ee
Aniket <aniketsahu1711@gmail.com>
2023-07-09 17:43:12 +0530

    Checking render without ball model

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas,Footer } from "./components";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas, Footer } from "./components";
 
 const App = () => {
   return (
@@ -11,7 +11,7 @@ const App = () => {
         </div>
         <About />
         {/* <Experience /> */}
-        <Tech />
+        {/* <Tech /> */}
         <Works />
         {/* <Feedbacks /> */}
         <div className=" realtive z-0">
